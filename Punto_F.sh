590e2613b4df54f4972e69ab0257373b7671d720
Pedro Perez Gambale <pedroperezgambale.utn@gmail.com>
2026-05-19 19:46:28 +0000

    ADD: Punto_F.sh

---
+++ Punto_F.sh
@@ -0,0 +1,6 @@
+mkdir -p ~/Punto_F
+IP_PUB=186.158.220.73
+echo "Mi ip publica es: " > ~/Punto_F/Filtro_Avanzado.txt
+MODELO=1600
+FRECUENCIA=
+echo "CPU Modelo:  Frecuencia: " >> ~/Punto_F/Filtro_Avanzado.txt
