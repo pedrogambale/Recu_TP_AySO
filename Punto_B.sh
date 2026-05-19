2ab072724cab12b1ff11de330ae182dfbd3c9af3
Pedro Perez Gambale <pedroperezgambale.utn@gmail.com>
2026-05-19 19:20:42 +0000

    ADD: Punto_B.sh

--- Punto_B.sh
+++ Punto_B.sh
@@ -3,4 +3,4 @@ sudo useradd -m -s /bin/bash -g p1c1_2026_u1 p1c1_2026_u1
 echo "p1c1_2026_u1:clave123" | sudo chpasswd
 sudo mkdir -p /datos
 sudo chown p1c1_2026_u1:p1c1_2026_u1 /datos
-sudo -u p1c1_2026_u1 bash -c "whoami > /datos/validar1.txt
+sudo -u p1c1_2026_u1 bash -c "whoami > /datos/validar1.txt"
